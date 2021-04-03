9b2611cdcd055b274776b1d18c416cd36dc55118
nharikrishna <nharikrishna275@gmail.com>
2021-04-03 18:59:35 +0530

    Extra features

--- calculator/mainapp/views.py
+++ calculator/mainapp/views.py
@@ -92,7 +92,7 @@ def class_details_view(request):
 def view_course_advisor_view(request):
     username = request.user.username
     entry1 = Advisor.objects.get(username=username)
-    entry2 = Course.objects.filter(year=entry1.year)
+    entry2 = Course.objects.all()
     context = {'e1': entry1, 'e2': entry2}
     return render(request, 'Advisor/view_course.html', context)
 
@@ -109,7 +109,23 @@ def view_marks_advisor_view(request):
     username = request.user.username
     entry1 = Advisor.objects.get(username=username)
     entry2 = Course.objects.filter(year=entry1.year)
-    context = {'e1': entry1, 'e2': entry2}
+    entry3 = Student.objects.filter(year=entry1.year)
+
+    roll = {}
+    for i in entry3:
+        roll[i.username] = i.roll_number
+    print(roll)
+
+    course_mark = {}
+    for i in entry2:
+        course_mark[i.course_title] = {}
+        entry4 = Mark.objects.filter(course_title=i.course_title)
+        for j in entry4:
+            course_mark[i.course_title][j.username_id] = j.marks
+
+    print(course_mark)
+
+    context = {'e1': entry1, 'e2': entry2, 'e3': entry3, 'roll': roll, 'mark': course_mark}
     return render(request, 'Advisor/view_marks.html', context)
 
 
@@ -162,18 +178,84 @@ def view_course_update_faculty_view(request):
 def view_marks_faculty_view(request, course_title):
     username = request.user.username
     entry1 = Faculty.objects.get(username=username)
-    entry2 = Mark.objects.filter(course_title=course_title)
-    context = {'e1': entry1, 'e2': entry2, 'course_title': course_title}
+    entry_year = Course.objects.get(course_title=course_title)
+    year = entry_year.year
+    entry2 = Course.objects.filter(year=year)
+    entry3 = Student.objects.filter(year=year)
+
+    course_mark = {}
+    for i in entry2:
+        course_mark[i.course_title] = {}
+        entry4 = Mark.objects.filter(course_title=i.course_title)
+        for j in entry4:
+            course_mark[i.course_title][j.username_id] = j.marks
+
+    print(course_mark)
+
+    roll = {}
+    name = {}
+    for i in entry3:
+        roll[i.username] = i.roll_number
+        name[i.username] = i.first_name
+
+    context = {'e1': entry1, 'e2': entry2, 'e3': entry3, 'course_title': course_title, 'mark': course_mark, 'name': name, 'roll': roll}
     return render(request, 'Faculty/view_marks.html', context)
 
 
 def update_marks_view(request, course_title):
+    if request.method == 'POST':
+        entry_year = Course.objects.get(course_title=course_title)
+        year = entry_year.year
+        entry = Student.objects.filter(year=year)
+        d = {}
+        for i in entry:
+            d[i.username] = request.POST.get(i.username)
+        print(d)
+        for i in d:
+            row_entry = Mark.objects.get(username=i, course_title=course_title)
+            row_entry.marks = d[i]
+            row_entry.save(update_fields=['marks'])
+        messages.success(request, 'Form Submitted successfully!')
+
     username = request.user.username
     entry1 = Faculty.objects.get(username=username)
-    entry2 = Mark.objects.filter(course_title=course_title)
+    entry_year = Course.objects.get(course_title=course_title)
+    year = entry_year.year
+    entry2 = Course.objects.filter(year=year)
+    entry3 = Student.objects.filter(year=year)
 
-    context = {'e1': entry1, 'e2': entry2, 'course_title': course_title}
+    course_mark = {}
+    for i in entry2:
+        course_mark[i.course_title] = {}
+        entry4 = Mark.objects.filter(course_title=i.course_title)
+        for j in entry4:
+            course_mark[i.course_title][j.username_id] = j.marks
+
+    roll = {}
+    name = {}
+    for i in entry3:
+        roll[i.username] = i.roll_number
+        name[i.username] = i.first_name
+
+    context = {'e1': entry1, 'e2': entry2, 'e3': entry3, 'course_title': course_title, 'mark': course_mark,
+               'name': name, 'roll': roll}
     return render(request, 'Faculty/update_marks.html', context)
+
+
+def view_course_grade_view(request):
+    username = request.user.username
+    entry1 = Faculty.objects.get(username=username)
+    entry2 = Course.objects.all()
+    context = {'e1': entry1, 'e2': entry2}
+    return render(request, 'Faculty/view_course_grade.html', context)
+
+
+def view_grade_faculty_view(request, course_title):
+    username = request.user.username
+    entry1 = Faculty.objects.get(username=username)
+    entry2 = Grade.objects.get(course_title=course_title)
+    context = {'e1': entry1, 'e2': entry2}
+    return render(request, 'Faculty/view_grade.html', context)
 
 
 def view_profile_coe_view(request):
@@ -194,6 +276,33 @@ def view_course_coe_view(request):
 
 
 def update_grade_view(request, course_title):
+    if request.method == 'POST':
+        entry = Grade.objects.filter(course_title=course_title)
+        fromA = request.POST.get('fromA')
+        toA = request.POST.get('toA')
+        fromB = request.POST.get('fromB')
+        toB = request.POST.get('toB')
+        fromC = request.POST.get('fromC')
+        toC = request.POST.get('toC')
+        fromF = request.POST.get('fromF')
+        toF = request.POST.get('toF')
+        credit = request.POST.get('credit')
+        print(fromA, toA, fromB, toB, fromC, toC, fromF, toF, credit)
+
+        row_entry = Grade.objects.get(course_title=course_title)
+        row_entry.fromA = fromA
+        row_entry.fromB = fromB
+        row_entry.fromC = fromC
+        row_entry.fromF = fromF
+        row_entry.toA = toA
+        row_entry.toB = toB
+        row_entry.toC = toC
+        row_entry.toF = toF
+        row_entry.credits = credit
+
+        row_entry.save(update_fields=['fromA', 'fromB', 'fromC', 'fromF', 'toA', 'toB', 'toC', 'toF', 'credits'])
+        messages.success(request, 'Form Submitted successfully!')
+
     username = request.user.username
     entry1 = Coe.objects.get(username=username)
 
